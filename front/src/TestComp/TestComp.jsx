0be45d48c43fb038228bea674e295f6b170b71cf
Jean Vuillet <vuillet433@hotmail.com>
2024-03-21 20:03:35 -0500

    imported data

--- front/src/TestComp/TestComp.jsx
+++ front/src/TestComp/TestComp.jsx
@@ -1,39 +1,78 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import * as d3 from 'd3';
 import './TestComp.scss';
+
+import {    USER_MAIN_DATA,
+    USER_ACTIVITY,
+    USER_AVERAGE_SESSIONS,
+    USER_PERFORMANCE} from './../data/data'
 
 
 export function TestComp() {
     const canvasRef = useRef(null);
-    const [data, setData] = useState(null);
+
+
 
     useEffect(() => {
-        const fetchData = async () => {
-            d3.select(canvasRef.current).selectAll("*").remove();
+        d3.select(canvasRef.current).selectAll("*").remove();
 
-            try {
-                const response = await fetch('../../../back/app/data.js');
-                if (!response.ok) {
-                    throw Error('La requête a échoué ' + response.status);
-                }
 
-                const text = await response.clone().text();
-                const script = new Function(text);
-                const newData = script();
-                setData(newData);
-            } catch (error) {
-                console.error('Erreur lors de la récupération des données :', error);
-            }
-        };
+      
 
-        fetchData();
-    }, []);
 
-    useEffect(() => {
-        if (data) {
-            // Votre code D3 ici...
+        const width = 700;
+        const height = 200;
+        const margin = { top: 20, right: 30, bottom: 30, left: 40 };
+const data=USER_ACTIVITY[0].sessions;
+        const values=[];
+        for (let i=0; i<data.length; i++){
+            values.push(data[i].kilogram)
         }
-    }, [data]);
+
+
+        const x = d3.scaleBand() // Utiliser une échelle de bande pour les valeurs discrètes
+            .domain(d3.range(values.length)) // Domaine basé sur le nombre d'éléments dans le tableau
+            .range([0, width - margin.left - margin.right])
+            .padding(0.1); // Espacement entre les barres
+
+        const y = d3.scaleLinear()
+            .domain([0, d3.max(values)]) // Domaine basé sur les valeurs de votre tableau
+            .range([height - margin.top - margin.bottom, 0]);
+
+        const svg = d3.select(canvasRef.current)
+            .append("svg")
+            .attr("width", width)
+            .attr("height", height)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        svg.selectAll("rect")
+            .data(values)
+            .enter()
+            .append("rect")
+            .attr("x", (d, i) => x(i)) // Utiliser l'indice i pour positionner les barres sur l'axe x
+            .attr("y", d => y(d))
+            .attr("width", 7) // Largeur basée sur la largeur de la bande
+            .attr("height", d => height - margin.top - margin.bottom - y(d))
+            .attr("fill", "#282D30");
+
+        const xAxis = d3.axisBottom(x)
+        .ticks(10)
+            .tickFormat(d3.format('d')); // Utiliser les valeurs de votre tableau pour les étiquettes de l'axe x
+
+        const yAxis = d3.axisLeft(y)
+            .ticks(10)
+            .tickFormat(d3.format("d"));
+
+        svg.append("g")
+            .attr("transform", "translate(0," + (height - margin.top - margin.bottom) + ")")
+            .call(xAxis);
+
+        svg.append("g")
+            .call(yAxis);
+      
+    }, []);
 
     return <div className='leDiv' ref={canvasRef}></div>;
 }
+
