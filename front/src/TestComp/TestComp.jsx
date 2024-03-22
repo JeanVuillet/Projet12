cbd51b96eb0a7809ebd2403bf13bf7972c8ede22
Jean Vuillet <vuillet433@hotmail.com>
2024-03-22 12:02:37 -0500

    first half graphic 1

--- front/src/TestComp/TestComp.jsx
+++ front/src/TestComp/TestComp.jsx
@@ -1,44 +1,49 @@
-import React, { useEffect, useRef } from 'react';
-import * as d3 from 'd3';
-import './TestComp.scss';
-
-import {    USER_MAIN_DATA,
-    USER_ACTIVITY,
-    USER_AVERAGE_SESSIONS,
-    USER_PERFORMANCE} from './../data/data'
-
+import React, { useEffect, useRef } from 'react'; // Importation des hooks React nécessaires
+import * as d3 from 'd3'; // Importation de D3.js pour la visualisation des données
+import './TestComp.scss'; // Importation du fichier de style CSS
+import { USER_ACTIVITY } from './../data/data'; // Importation des données utilisateur
 
 export function TestComp() {
-    const canvasRef = useRef(null);
+    const canvasRef = useRef(null); // Création d'une référence pour le composant Canvas
 
+    useEffect(() => { // Utilisation de useEffect pour effectuer des opérations lors du montage du composant
+        d3.select(canvasRef.current).selectAll("*").remove(); // Suppression de tout contenu précédent dans le composant Canvas
 
-
-    useEffect(() => {
-        d3.select(canvasRef.current).selectAll("*").remove();
-
-
-      
-
-
+        // Définition des dimensions du graphique et des marges
         const width = 700;
         const height = 200;
         const margin = { top: 20, right: 30, bottom: 30, left: 40 };
-const data=USER_ACTIVITY[0].sessions;
-        const values=[];
-        for (let i=0; i<data.length; i++){
-            values.push(data[i].kilogram)
+        const leftGap=40;
+
+        // Récupération des données d'activité utilisateur
+        const data = USER_ACTIVITY[0].sessions;
+        console.log(data);
+        let kilograms=[];
+        let calories=[]
+        for( let i=0; i<data.length; i++){
+            kilograms.push(data[i].kilogram)
+            calories.push(data[i].calories)
         }
 
+        console.log(kilograms);
+        console.log(calories);
+    
+        // Création du tableau de données pour les barres
+        const list2 = [69, 73, 81, 80, 79, 78, 66];
 
-        const x = d3.scaleBand() // Utiliser une échelle de bande pour les valeurs discrètes
-            .domain(d3.range(values.length)) // Domaine basé sur le nombre d'éléments dans le tableau
-            .range([0, width - margin.left - margin.right])
-            .padding(0.1); // Espacement entre les barres
-
+        // Création de l'échelle pour l'axe x (échelle lineaire)
+        const x = d3.scaleLinear()
+            .domain([1,7]) 
+            .range([0, width - margin.left - margin.right])//taille en px de laxe des x
+        
+            const minYValue = Math.min(...kilograms);
+            const maxYValue=Math.max(...kilograms);
+        // Création de l'échelle pour l'axe y (échelle linéaire)
         const y = d3.scaleLinear()
-            .domain([0, d3.max(values)]) // Domaine basé sur les valeurs de votre tableau
-            .range([height - margin.top - margin.bottom, 0]);
+            .domain([minYValue-1,maxYValue+1]) // Définition du domaine de 65 à 85 pour l'axe y
+            .range([height - margin.top - margin.bottom ,0]);
 
+        // Création de l'élément SVG et du conteneur graphique
         const svg = d3.select(canvasRef.current)
             .append("svg")
             .attr("width", width)
@@ -46,33 +51,43 @@ const data=USER_ACTIVITY[0].sessions;
             .append("g")
             .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+        // Création des barres rectangulaires
         svg.selectAll("rect")
-            .data(values)
+            .data(kilograms)
             .enter()
             .append("rect")
-            .attr("x", (d, i) => x(i)) // Utiliser l'indice i pour positionner les barres sur l'axe x
+            .attr("x", (d, i) => x(i+1))
             .attr("y", d => y(d))
-            .attr("width", 7) // Largeur basée sur la largeur de la bande
+            .attr("width", 7)
             .attr("height", d => height - margin.top - margin.bottom - y(d))
             .attr("fill", "#282D30");
 
+        // Création de l'axe x
         const xAxis = d3.axisBottom(x)
-        .ticks(10)
-            .tickFormat(d3.format('d')); // Utiliser les valeurs de votre tableau pour les étiquettes de l'axe x
+            .tickValues(d3.range(1, 8)) // Définition des valeurs de l'axe x de 1 à 7
+            .tickFormat(d3.format('d')); // Formatage des étiquettes de l'axe x
 
+        // Création de l'axe y
         const yAxis = d3.axisLeft(y)
-            .ticks(10)
-            .tickFormat(d3.format("d"));
+        .tickValues([minYValue, (minYValue + maxYValue+1) / 2, maxYValue+1,]) 
+            .tickFormat(d3.format("d"))
+ 
 
+        // Ajout de l'axe x au graphique
         svg.append("g")
             .attr("transform", "translate(0," + (height - margin.top - margin.bottom) + ")")
             .call(xAxis);
 
+        // Ajout de l'axe y au graphique
         svg.append("g")
-            .call(yAxis);
-      
-    }, []);
+        .attr("transform", "translate(" + (width - margin.left - margin.right+leftGap) + ",0)")
+        .call(yAxis)
+     
+        .selectAll("line, path")
+        .attr("stroke", "transparent"); // Rendre les lignes de l'axe invisibles;
 
+    }, []); // La dépendance vide indique que cette fonction useEffect ne s'exécute qu'une seule fois après le montage initial du composant
+
+    // Rendu du composant Canvas
     return <div className='leDiv' ref={canvasRef}></div>;
 }
-
