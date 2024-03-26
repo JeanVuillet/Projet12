7385f83cca5d96eecd11462eee932305c1b01322
Jean Vuillet <vuillet433@hotmail.com>
2024-03-26 18:16:17 -0500

    ok

--- front/src/TestComp/TestComp.jsx
+++ front/src/TestComp/TestComp.jsx
@@ -9,48 +9,48 @@ import {
     ResponsiveContainer
 } from 'recharts';
 import { USER_ACTIVITY } from './../data/data';
-import'./TestComp.scss';
+import './TestComp.scss';
 
 export function TestComp() {
     const data = USER_ACTIVITY[0].sessions.map((session, index) => ({
-        name: `Session ${index + 1}`,
+        name: `${index + 1}`,
         kilograms: session.kilogram,
         calories: session.calories,
     }));
 
     return (
-        <div className='leDiv' style={{ width: '100%', height: 400 }}>
+        <div className='leDiv' style={{ width: '500px', height: '400px' }}>
             <ResponsiveContainer>
                 <BarChart
                     data={data}
                     margin={{ top: 20, right: 30, left: 40, bottom: 30 }}
+                    barSize={8} // Définir la largeur des barres à 8 pixels
                 >
-  
-  <YAxis
-  yAxisId="right"
-  orientation="right"
-  domain={['dataMin', 'dataMax']}
-  tick={({ x, y, payload }) => (
-    <text
-      x={x+15}
-      y={y}
-      dy={4}
-      textAnchor="start"
-      fill="#666"
-      className="couou"
-    >
-      {payload.value}
-    </text>
-  )}
-/>
-                    
-                    <YAxis yAxisId="right" orientation="right" />
+                    <XAxis dataKey="name" />
+                    <YAxis
+                        yAxisId="right"
+                        orientation="right"
+                        domain={[0, Math.ceil(Math.max(...data.map(entry => entry.calories)) / 4.81)]} // Définir le domaine personnalisé
+                        tick={({ x, y, payload }) => (
+                            <text
+                                x={x + 15}
+                                y={y}
+                                dy={4}
+                                textAnchor="start"
+                                fill="#666"
+                                className="couou"
+                            >
+                                {Math.round(payload.value * 4.81)} {/* Conversion des calories en pixels */}
+                            </text>
+                        )}
+                    />
                     <Tooltip />
                     <CartesianGrid strokeDasharray="3 3" vertical={false} /> {/* Seulement les lignes horizontales */}
                     <Bar dataKey="calories" fill="#E60000" yAxisId="right" />
-                    <Bar dataKey="kilograms" fill="#E60000" yAxisId="right" />
+                    <Bar dataKey="kilograms" fill="#282D30" yAxisId="right" />
                 </BarChart>
             </ResponsiveContainer>
         </div>
     );
 }
+
