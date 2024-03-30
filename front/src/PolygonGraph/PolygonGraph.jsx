203bd465a84061da6b1583ffb0a4860bc5cb20a3
Jean Vuillet <vuillet433@hotmail.com>
2024-03-30 12:11:16 -0500

    ok

--- front/src/PolygonGraph/PolygonGraph.jsx
+++ front/src/PolygonGraph/PolygonGraph.jsx
@@ -1,23 +1,24 @@
+import React from "react";
 import { ResponsiveContainer, PolarAngleAxis, PolarGrid, Radar, RadarChart, Tooltip } from "recharts";
+import { USER_PERFORMANCE } from "../data/data";
 import './PolygonGraph.scss';
 
-
-const data = [
-  { subject: 'Math', A: 120, B: 110, fullMark: 150 },
-  { subject: 'English', A: 98, B: 130, fullMark: 150 },
-  { subject: 'Science', A: 86, B: 130, fullMark: 150 },
-  { subject: 'History', A: 99, B: 100, fullMark: 150 },
-  { subject: 'Geography', A: 85, B: 90, fullMark: 150 },
-];
-
 export function PolygonGraph() {
+  const mydata = USER_PERFORMANCE[0].data;
+  const mySubject = ['Cardio', 'Energie', 'Endurance', 'Force', 'Vitesse', 'Intensité'];
+
+  const values = mydata.map((element, index) => ({
+    subject: mySubject[index],
+    value: element.value
+  }));
+  const newValues=values.reverse();
   return (
     <div className="polygonGraph">
       <ResponsiveContainer width="100%" height={400}>
-        <RadarChart cx="50%" cy="50%" outerRadius="80%" data={data}>
+        <RadarChart cx="50%" cy="50%" outerRadius="80%" data={newValues}>
           <PolarGrid gridType='polygon' radialLines={false} polarRadius={[0, 30, 60, 90, 120, 150]} />
           <PolarAngleAxis dataKey="subject" />
-          <Radar name="Mike" dataKey="A"  fill="#FF0101" fillOpacity={0.7} />
+          <Radar name="Mike" dataKey="value"  fill="#FF0101" fillOpacity={0.7} />
 
           <Tooltip />
         </RadarChart>
