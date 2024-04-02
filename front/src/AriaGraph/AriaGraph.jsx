d2cce2c8557350de9364190bc5f2f3519f672d56
Jean Vuillet <vuillet433@hotmail.com>
2024-04-02 11:21:01 -0500

    ok

--- front/src/AriaGraph/AriaGraph.jsx
+++ front/src/AriaGraph/AriaGraph.jsx
@@ -6,16 +6,17 @@ import './AriaGraph.scss';
 export function AriaGraph() {
   const data2 = USER_AVERAGE_SESSIONS[0].sessions;
   const newData = data2.map((element) => ({ day: element.day, length: element.sessionLength }));
-
+  const maxValue = Math.max(newData);
+  const minValue = Math.min(newData);
   return (
     <div className='containerDiv'>
       <ResponsiveContainer width={258} height={260} className='container'>
         <AreaChart data={newData} margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
 
           <XAxis dataKey="day" />
-          <YAxis />
-          <Tooltip />
-          <Area type="monotone" dataKey="length" stroke="white" fill="none" strokeLinecap="round" />
+     <YAxis  domain={[minValue,maxValue]}/>
+     <Tooltip label="length" />
+          <Area  dataKey="length" stroke="white" fill="none" strokeLinecap="round" type='natural' />
         </AreaChart>
       </ResponsiveContainer>
     </div>
