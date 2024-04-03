65dc5f6d4aab59674808d22021ed9034aeecdc6f
Jean Vuillet <vuillet433@hotmail.com>
2024-04-03 12:27:56 -0500

    api working

--- front/src/Api/Api.jsx
+++ front/src/Api/Api.jsx
@@ -1,30 +1,33 @@
-import './Api.scss';
-import { useState, useEffect } from 'react';
+import { index } from "d3";
+import "./Api.scss";
+import { useState, useEffect } from "react";
 
-export function Api({userId}){
+export function Api({ userId,getData }) {
+  let [userData, setUserData] = useState(null);
 
-    const [userData, setUserData] = useState(null);
+  useEffect(() => {
+    // Appel à la route '/user/:id' de votre backend
+    fetch(`http://localhost:3000/user/${userId}/activity`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Erreur lors de la récupération des données");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setUserData((userData = data)); // Affichez les données dans la console
+        // Assurez-vous de définir les données correctement
+       getData(data);
+      })
+      .catch((error) => console.error(error)).then(
+     
+      ) // Gérez les erreurs de manière appropriée
+  }, [userId]); // Assurez-vous de dépendre de userId pour rafraîchir les données si nécessaire
 
-    useEffect(() => {
-        // Appel à la route '/user/:id' de votre backend
-        fetch(`/user/${userId}/activity`)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Erreur lors de la récupération des données');
-                }
-          
-            })
-            .then(data => {
-// Assurez-vous de définir les données correctement
-                console.log('voilqdata'); // Affichez les données dans la console
-            })
-            .catch(error => console.error(error)); // Gérez les erreurs de manière appropriée
-    }, [userId]); // Assurez-vous de dépendre de userId pour rafraîchir les données si nécessaire
+  // Affichez les données dans votre composant
+  return (
+    <div className="api">
   
-    // Affichez les données dans votre composant
-    return (
-        <div className="api">
-            {/* Insérez ici votre code pour afficher les données */}
-        </div>
-    );
+    </div>
+  );
 }
