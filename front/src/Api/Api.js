264c2a8153b8462542075574a6ca56a3e562dac7
Jean Vuillet <vuillet433@hotmail.com>
2024-04-05 20:45:50 -0500

    apis better

--- front/src/Api/Api.js
+++ front/src/Api/Api.js
@@ -15,3 +15,55 @@ return data;// Appelle getData avec les données mises à jour
     console.error(error);
   }
 }
+
+export async function userApi(userId){
+
+  try {
+  const response =await fetch (`http://localhost:3000/user/${userId}`)
+  if (!response.ok) {
+    throw new Error("Erreur lors de la récupération des données");
+  }
+
+  const data= await response.json(); 
+
+  return data;
+}
+catch (error) {
+  console.error(error);
+}
+
+}
+
+export async function perfApi(userId){
+  
+  try{
+  const response = await fetch(`http://localhost:3000/user/${userId}/performance`)
+
+  if(! response.ok) {
+    throw new Error('data non reçu')
+  }
+  const data= await response.json();
+  return data
+}
+catch(error){
+  console.log(error)
+}
+    
+}
+
+export async  function averageSessions(){
+  try{
+    const response = await fetch(`http://localhost:3000/user/${userId}/average-sessions`)
+  
+    if(! response.ok) {
+      throw new Error('data non reçu')
+    }
+    const data= await response.json();
+    return data
+  }
+  catch(error){
+    console.log(error)
+  }
+    
+
+}
--- front/src/FormatData/dataFormater.js
+++ front/src/FormatData/dataFormater.js
@@ -1,48 +1,160 @@
-import { apiActivity } from "../Api/Api.js"
+import { apiActivity, userApi,perfApi } from "../Api/Api.js";
 import { data } from "react";
-import { USER_MAIN_DATA,USER_ACTIVITY,USER_AVERAGE_SESSIONS,USER_PERFORMANCE} from '../data/data.js'
+import {
+  USER_MAIN_DATA,
+  USER_ACTIVITY,
+  USER_AVERAGE_SESSIONS,
+  USER_PERFORMANCE,
+} from "../data/data.js";
 
 import { DataSelector } from "../DataSelect/SelectPage.jsx";
 
-
-export  class User {
-
-constructor(origin,id){
-
+export class User {
+  constructor(origin, id) {
     this.origin = origin;
-    this.id=id;
-             }
-  
+    this.id = id;
+  }
 
-
-
-
-
- 
-         async getActivity(){
-
-        if(this.origin==='api'){
-        try{
-        const data= await apiActivity(this.id);
+  async getActivity() {
+    if (this.origin === "api") {
+      try {
+        const data = await apiActivity(this.id);
         if (!data) {
-          throw new Error('Les données de l\'API sont vides');
-        }
-        else(console.log('dataIs'+data))
+          throw new Error("Les données de l'API sont vides");
+        } else console.log("dataIs" + data);
 
-        const sessions= await data.data.sessions
-        console.log(console.log(Object.keys(data), sessions))
+        const sessions = await data.data.sessions;
+        console.log(console.log(Object.keys(data), sessions));
 
         return data.data.sessions;
-        } catch(error){
-            console.log(error)
-            return null;
-        }
-      }else if( this.origin==='mock'){
-        const data=USER_ACTIVITY;
-        const thisData=data.find((element)=>(element.userId=this.id))
-        return thisData.sessions;
+      } catch (error) {
+        console.log(error);
+        return null;
       }
-    
-      }
+    } else if (this.origin === "mock") {
+      const data = USER_ACTIVITY;
+      const thisData = data.find((element) => (element.userId = this.id));
+      return thisData.sessions;
     }
+  }
 
+  async getUserName(){
+
+    if(this.origin === "api"){
+      try{
+const data= await  userApi(this.id)
+if(!data.ok){
+  throw new Error("Couldn't get user Data Info")
+}
+const name=  await data.data.userInfos.firstName;
+
+console.log(console.log(Object.keys(data),name));
+return name;
+      }
+      catch (error) {
+        console.log(error);
+        return null;
+    }
+  } else if (this.origin === "mock") {
+const data=USER_MAIN_DATA;
+const thisData = data.find((element) => (element.userId = this.id));
+return thisData.userInfos.firstName;
+
+  }
+}
+
+async getScore(){
+
+  if(this.origin === "api"){
+    try{
+const data= await  userApi(this.id)
+if(!data.ok){
+throw new Error("Couldn't get user Data Info")
+}
+const todayScore=  await data.data.userInfos.todayScore;
+
+console.log(console.log(Object.keys(data),todayScore));
+return todayScore;
+    }
+    catch (error) {
+      console.log(error);
+      return null;
+  }
+} else if (this.origin === "mock") {
+const data=USER_MAIN_DATA;
+const thisData = data.find((element) => (element.userId = this.id));
+return thisData.todayScore;
+
+}
+}
+async getKeyData(){
+
+  if(this.origin === "api"){
+    try{
+const data= await  userApi(this.id)
+if(!data.ok){
+throw new Error("Couldn't get user Data Info")
+}
+const keyData=  await data.data.userInfos.keyData;
+
+console.log(console.log(Object.keys(data),keyData));
+return keyData;
+    }
+    catch (error) {
+      console.log(error);
+      return null;
+  }
+} else if (this.origin === "mock") {
+const data=USER_MAIN_DATA;
+const thisData = data.find((element) => (element.userId = this.id));
+return thisData.keyData;
+
+}
+}
+
+async getPerformance(){
+  if(this.origin === "api"){
+    try{
+const data= await  perfApi(this.id)
+if(!data.ok){
+throw new Error("Couldn't get user Data Info")
+}
+const performance=  await data.data;
+
+console.log(console.log(Object.keys(data),performance));
+return performance;
+    }
+    catch (error) {
+      console.log(error);
+      return null;
+  }
+} else if (this.origin === "mock") {
+const data=USER_MAIN_DATA;
+const thisData = data.find((element) => (element.userId = this.id));
+return thisData;
+}
+}
+
+async getAverageSessions(){
+  if(this.origin === "api"){
+    try{
+const data= await  userApi(this.id)
+if(!data.ok){
+throw new Error("Couldn't get user Data Info")
+}
+const averageSession=  await data.data.sessions;
+
+console.log(console.log(Object.keys(data),averageSession));
+return averageSession;
+    }
+    catch (error) {
+      console.log(error);
+      return null;
+  }
+} else if (this.origin === "mock") {
+const data=USER_AVERAGE_SESSIONS;
+const thisData = data.find((element) => (element.userId = this.id));
+return thisData.sessions;
+}
+}
+}
