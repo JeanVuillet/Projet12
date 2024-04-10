8e7d7ef1c160cbf1545ab9370f2e0563d058f922
Jean Vuillet <vuillet433@hotmail.com>
2024-04-10 11:05:43 -0500

    dell imports

--- front/src/index.js
+++ front/src/index.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { Header } from './MainComp/Header/Header.jsx';
-import { SideBarre } from './MainComp/SideBarre/SideBarre.jsx';
 import reportWebVitals from './reportWebVitals';
-import { BarGraph } from './Graphs/BarGraph/BarGraph.jsx';
-import { Welcome } from './MainComp/MainDiv/Welcome/Welcome.jsx';
-import { MainDiv } from './MainComp/MainDiv/MainDiv.jsx';
 import { DataProvider } from './DataProvider/DataProvider.jsx';
 import { MainComp } from './MainComp/MainComp.jsx';
 
