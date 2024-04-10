8b3cfba2478caa250f89bb2846db8434a3ed24ed
Jean Vuillet <vuillet433@hotmail.com>
2024-04-10 09:36:52 -0500

    Dov

--- front/src/MainComp/MainComp.jsx
+++ front/src/MainComp/MainComp.jsx
@@ -1,5 +1,5 @@
 import { Header } from "./Header/Header.jsx";
-import { MainDiv } from "./MainDiv/MainDiv.jsx";
+import { MainDiv } from "./MainDiv/MainDov.jsx";
 import { SelectPage } from "./SelectPage/SelectPage.jsx";
 import { SideBarre } from "./SideBarre/SideBarre.jsx";
 
